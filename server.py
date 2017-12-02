535184dc35f193c360406dadb557757a67feb6de
Samat Karymshakov <bskordo@gmail.com>
2017-12-02 13:51:42 +0600

    added link for movie

--- server.py
+++ server.py
@@ -13,17 +13,17 @@ cache = SimpleCache()
 
 def get_movies_from_cache():
     movies = cache.get('movies')
-    avisha_movie = cache.get('avisha_movie')
-    if movies or avisha_movie is None:
-        movies, avisha_movie = collect_movies_information()
+    afisha_link = cache.get('afisha_link')
+    if movies or afisha_link is None:
+        movies, afisha_link = collect_movies_information()
         cache.set('movies', movies, timeout=86400)
-        cache.set('avisha_movie', avisha_movie, timeout=86400)
-    return movies ,avisha_movie
+        cache.set('afisha_link', afisha_link, timeout=86400)
+    return movies ,afisha_link
 
 
 @app.route('/todo/api/full_information.json', methods=['GET'])
 def get_api():
-    movies,avisha_movie = get_movies_from_cache()
+    movies,afisha_link = get_movies_from_cache()
     return Response(json.dumps(movies,indent=4),
                     content_type='application/json; charset=utf-8')
 
@@ -35,9 +35,9 @@ def api_description():
 
 @app.route('/')
 def films_list():
-    movies, avisha_movie = get_movies_from_cache()
+    movies, afisha_link = get_movies_from_cache()
     return render_template('films_list.html',movies = movies,
-                                            avisha_movie = avisha_movie)
+                                            afisha_link = afisha_link)
    
 
 if __name__ == "__main__":
